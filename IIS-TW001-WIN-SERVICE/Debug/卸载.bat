9eb861fda61e002e76cbd13397d19bbcb51c09e5
Mark Chenpingling <chenpingling@gmail.com>
2020-10-09 11:19:20 +0800

    fix 2

--- IIS-TW001-WIN-SERVICE/Debug/卸载.bat
+++
@@ -1,1 +0,0 @@
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe -u C:\2020\projects\TAIWAY\0915\branches\Web\DreamTek.WinService\bin\Debug\DreamTek.WinService.exe
--- IIS-TW001-WIN-SERVICE/Debug/安装.bat
+++
@@ -1,2 +0,0 @@
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe  C:\2020\projects\TAIWAY\0915\branches\Web\DreamTek.WinService\bin\Debug\DreamTek.WinService.exe
-pause
